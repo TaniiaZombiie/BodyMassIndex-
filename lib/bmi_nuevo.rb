b35a0323f4fbd019e8818467819f1ea7319bd6bf
TaniiaZombiie <11460260@itcolima.edu.mx>
2015-12-14 19:30:32 -0600

    Correccion

--- lib/bmi_nuevo.rb
+++ lib/bmi_nuevo.rb
@@ -1,25 +1,14 @@
 require "bmi_nuevo/version"
 
 module BmiNuevo
-{
-
 	class bmi
-
 		attr_reader :peso, :altura
-
 		def inicializar(peso,altura)
-
 		   @peso, @altura = peso, altura
-
 		end
-
 		def calcular_bmi	
-
 		   (peso/(altura**2))
-
 		end
-
 	end
-
-}
 end
+
